c42abbbf8924692d1d005ea027a90a9dc52f8a1d
Le Xuan Dai <Dyan@nexondv.com>
2023-08-30 10:36:25 +0700

    graphql-mesh-docs

---
+++ packages/books-service/src/app.controller.ts
@@ -0,0 +1,60 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AppService } from './app.service';
+import { Book } from './entities/Book';
+import { Category } from './entities/Category';
+
+@ApiTags('books')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/books')
+  @ApiResponse({
+    status: 200,
+    type: [Book],
+  })
+  books() {
+    return this.appService.listBooks();
+  }
+
+  @Get('/categories')
+  @ApiResponse({
+    status: 200,
+    type: [Category],
+  })
+  categories() {
+    return this.appService.listBookCategories();
+  }
+
+  @ApiResponse({
+    status: 404,
+  })
+  @Get('/books/:id')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    type: Book,
+  })
+  book(@Param() params) {
+    return this.appService.findOneBook(params.id);
+  }
+
+  @Get('/categories/:id')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    type: Category,
+  })
+  category(@Param() params) {
+    return this.appService.findOneCategory(params.id);
+  }
+}
